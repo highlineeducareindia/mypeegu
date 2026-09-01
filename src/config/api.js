67089f6b7d6f16a53e1827bfddcaf9b5f3329ffb
patelsaab1 <patelsaab1@gmail.com>
2026-09-01 19:37:26 +0530

    Piva as counsellor response and chat age band update

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 const BASE_URL = "https://arsh.opserv.in/mypeegu/api"; 
-// const BASE_URL_DEV = "https://app.mypeegu.com/api";
-const BASE_URL_DEV = "http://localhost:3004/api";
+const BASE_URL_DEV = "https://app.mypeegu.com/api";
+// const BASE_URL_DEV = "http://localhost:3004/api";
 
 export const API_ENDPOINTS = {
   AWARDS: `${BASE_URL}/awards?action=fetch`, 
